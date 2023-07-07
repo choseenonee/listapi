cd24431504b831eb992ec693d7dc714bdd2a5f9a
kasse18 <fivstudio123@gmail.com>
2023-07-07 15:27:39 +0300

    misis.py

--- misis/misis.py
+++ misis/misis.py
@@ -1,9 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-s = '01.03.04'
-snils = '183-289-215 94'
-
 def misis(track: str, snils: str):
     fcode = ''.join(track.split('.'))
     r = requests.get(f'https://misis.ru/applicants/admission/progress/baccalaureate-and-specialties/list-of-applicants/list/?id=BAC-BUDJ-O-{fcode}-NITU_MISIS')
@@ -12,6 +9,5 @@ def misis(track: str, snils: str):
 
     for i in abit:
         if snils in i.text:
-            return(abit[abit.index(i)-2].text)
+            return(f'Ваше место: {abit[abit.index(i)-2].text}\nСнилс: {i.text}\nОбщая сумма баллов: {abit[abit.index(i)+2].text}\nСумма баллов по предметам: {abit[abit.index(i)+3].text}\nБаллы ИД: {abit[abit.index(i)+7].text}\nУсловие приема: {abit[abit.index(i)+9].text}')
     
-print(misis('01.03.04', '183-289-215 94'))
